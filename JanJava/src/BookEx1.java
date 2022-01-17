e40057c09f91ae2a00ed880096cff133391e0c96
Commonerd <92278529+Commonerd@users.noreply.github.com>
2022-01-17 17:07:06 +0900

    commit4 day0107

--- JanJava/src/BookEx1.java
+++ JanJava/src/BookEx1.java
@@ -2,6 +2,7 @@ class BookEx1 {
   public static void main(String[] args){
    System.out.println("test"); 
    System.out.println("test");
+    System.out.println("test");
     System.out.println("test"); 
   } 
 }
