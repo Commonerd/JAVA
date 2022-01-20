a8c9bfb14686d6f5dcaac1d4004c7bfe8b085870
commonerd <cornflake127@naver.com>
2022-01-20 17:18:32 +0900

    20220120목요일 7교시

--- JanJava/src/exception/day0120/PasswordException.java
+++ JanJava/src/exception/day0120/PasswordException.java
@@ -1,10 +1,13 @@
 package exception.day0120;
 
-public class PasswordException extends Exception{
+public class PasswordException extends IllegalArgumentException{
 
-	public PasswordException(String message) {
+	PasswordException(String message) {
 		super(message);
 	}
+	PasswordException(String password, String message){
+		super(password+" : "+message);
+	}//여기 생성자 2개 만듦. 
 }
 
 
--- JanJava/src/exception/day0120/PasswordTest.java
+++ JanJava/src/exception/day0120/PasswordTest.java
@@ -13,39 +13,39 @@ public class PasswordTest {
 			throw new PasswordException("비밀번호는 null일 수 없습니다.");
 		} else if (password.length()<5) {
 			throw new PasswordException("비밀번호는 5자 이상이어야 합니다.");
-	} else if (password.matches([a-zA-Z])==true) {
-		throw new PasswordException("비밀번호는 문자로만 이루어질 수 없습니다.");
-	}	
+	} else if (password.matches("[a-zA-Z]")) {
+		throw new PasswordException("비밀번호는 숫자를 포함해야 합니다.");
+	} 
 		this.password=password;
 }
 	
-	
 	public static void main(String[] args) {
-
-		PasswordTest pass = new PasswordTest();
+		PasswordTest pt = new PasswordTest();
 		try {
-			pass.setPassword(null);
+			pt.setPassword(null);
+			System.out.println(pt.getPassword());
+		} catch (PasswordException e) {
+			System.out.println(e.getMessage());
+		}
+		try {
+			pt.setPassword("abcd");
+			System.out.println(pt.getPassword());
+		} catch (PasswordException e) {
+			System.out.println(e.getMessage());
+		}
+		try {
+			pt.setPassword("abcdabcd");
+			System.out.println(pt.getPassword());
+		} catch (PasswordException e) {
+			System.out.println(e.getMessage());
+		}
+		try {
+			pt.setPassword("abcd1");
+			System.out.println(pt.getPassword());
 		} catch (PasswordException e) {
 			System.out.println(e.getMessage());
 		}
 		
-		try {
-			pass.setPassword(null);
-		} catch (PasswordException e) {
-			System.out.println(e.getMessage());
-		}
-		try {
-			pass.setPassword("1234");
-			System.out.println("저장된 비밀번호 : "+pass.getPassword());
-		} catch (PasswordException e) {
-			System.out.println(e.getMessage());
-		}
-		try {
-			pass.setPassword("abcdefg");
-			System.out.println("저장된 비밀번호 : "+pass.getPassword());
-		} catch (PasswordException e) {
-			System.out.println(e.getMessage());
-		}
 	}
 }
 		
